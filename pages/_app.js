4947e7bcda0f37e71579a0691c530f626ce29dfa
bohdan <bodyaalyas12@gmail.com>
2020-10-17 19:47:02 +0300

    added ssr for material

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,24 @@
+import {useEffect} from 'react'
 import '../styles/globals.css'
 import GlobalStyles from "../src/components/styled/globalStyles";
 import 'toastr/toastr.scss'
+import Head from "next/head";
 
 function MyApp({Component, pageProps}) {
-	return <>
-		<GlobalStyles/>
-		<Component {...pageProps} />
-	</>
+    useEffect(() => {
+        // Remove the server-side injected CSS.
+        const jssStyles = document.querySelector('#jss-server-side');
+        if (jssStyles) {
+            jssStyles.parentElement.removeChild(jssStyles);
+        }
+    }, []);
+    return <>
+        <GlobalStyles/>
+        <Head>
+            <title>Imagify</title>
+        </Head>
+        <Component {...pageProps} />
+    </>
 }
 
 export default MyApp
--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,21 @@
 import Document from 'next/document'
 import {ServerStyleSheet} from 'styled-components'
+import { ServerStyleSheets as MaterialStyleSheets } from '@material-ui/core/styles';
+
 
 export default class MyDocument extends Document {
     static async getInitialProps(ctx) {
         const sheet = new ServerStyleSheet()
+        const materialSheet = new MaterialStyleSheets()
         const originalRenderPage = ctx.renderPage
 
         try {
             ctx.renderPage = () =>
                 originalRenderPage({
                     enhanceApp: (App) => (props) =>
-                        sheet.collectStyles(<App {...props} />),
+                        sheet.collectStyles(
+                            materialSheet.collect(<App {...props} />)
+                        ),
                 })
 
             const initialProps = await Document.getInitialProps(ctx)
@@ -19,6 +24,7 @@ export default class MyDocument extends Document {
                 styles: (
                     <>
                         {initialProps.styles}
+                        {materialSheet.getStyleElement()}
                         {sheet.getStyleElement()}
                     </>
                 ),
